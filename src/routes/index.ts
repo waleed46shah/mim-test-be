45130ff0755e91e8b4f3185d8da05cc68c7b014a
Waleed Hassan <85634608+waleed46shah@users.noreply.github.com>
2025-01-19 14:59:59 +0500

    chore: delete extra files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 import userRoute from "./users";
 import authRoute from "./auth";
-import carRoute from "./car";
 
 const Route = express.Router();
 
 Route.use("/user", userRoute);
 Route.use("/auth", authRoute);
-Route.use("/car", carRoute);
 
 export default Route;
